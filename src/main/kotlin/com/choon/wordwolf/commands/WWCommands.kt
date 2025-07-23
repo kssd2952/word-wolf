e775c337f716240557f4e1b0eb19b810d546a7b5
choon52 <kssd2952@gmail.com>
2025-07-24 01:53:19 +0900

    명령어 정보 등을 한국어로 변경

--- src/main/kotlin/com/choon/wordwolf/commands/WWCommands.kt
+++ src/main/kotlin/com/choon/wordwolf/commands/WWCommands.kt
@@ -10,81 +10,81 @@ import org.bukkit.entity.Player
 class WWCommands : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) {
-            sender.sendMessage("This command can only be excuted by player")
+            sender.sendMessage("해당 명령어는 플레이어만 사용 가능합니다")
             return true
         }
 
         if (args.isEmpty()) {
-            sender.sendMessage("Usage: /ww <join|leave|list> [playerName]")
+            sender.sendMessage("잘못된 사용방법입니다")
             return true
         }
 
         when (args[0].lowercase()) {
             "join" -> {
                 if (args.size < 2) {
-                    sender.sendMessage("Usage: /ww <join|leave|list> [playerName]")
+                    sender.sendMessage("잘못된 사용방법입니다")
                     return true
                 }
                 val targetPlayer: Player? = Bukkit.getPlayer(args[1])
 
                 if (targetPlayer == null) {
-                    sender.sendMessage("Incorrect usage")
+                    sender.sendMessage("잘못된 사용방법입니다")
                 } else if (WordWolf.playerList.contains(targetPlayer)) {
-                    sender.sendMessage("Player already joined the game")
+                    sender.sendMessage("해당 플레이어는 이미 참여되었습니다")
                 } else {
                     WordWolf.playerList.add(targetPlayer)
-                    sender.sendMessage("${targetPlayer.name} has been added to the player list")
+                    sender.sendMessage("플레이어 ${targetPlayer.name}가 참여했습니다")
                 }
             }
 
             "leave" -> {
                 if (args.size < 2) {
-                    sender.sendMessage("Usage: /ww <join|leave|list> [playerName]")
+                    sender.sendMessage("잘못된 사용방법입니다")
                     return true
                 }
                 val targetPlayer: Player? = Bukkit.getPlayer(args[1])
 
                 if (targetPlayer == null) {
-                    sender.sendMessage("Incorrect usage")
+                    sender.sendMessage("잘못된 사용방법입니다")
                 } else if (!WordWolf.playerList.contains(targetPlayer)) {
-                    sender.sendMessage("Player have not joined the game")
+                    sender.sendMessage("해당 플레이어가 참여되어 있지 않습니다")
                 } else {
                     WordWolf.playerList.remove(targetPlayer)
-                    sender.sendMessage("${targetPlayer.name} has been removed from the player list")
+                    sender.sendMessage("플레이어 ${targetPlayer.name}를 참여 리스트에서 제거했습니다")
                 }
             }
 
             "rules" -> {
                 if (args.size < 2) {
-                    sender.sendMessage("Usage: /ww <join|leave|list> [playerName]")
+                    sender.sendMessage("잘못된 사용방법입니다")
                     return true
                 } else if (args.size == 2) {
                     val currentRule = WordWolf.gameRules[args[1]]
 
                     if (currentRule == null) {
-                        sender.sendMessage("There is no rule like that")
+                        sender.sendMessage("해당 규칙을 찾을 수 없습니다")
                     } else {
-                        sender.sendMessage("Current setting for ${args[1]} is $currentRule")
+                        sender.sendMessage("현재 ${args[1]} 게임 규칙은 ${currentRule}입니다")
                     }
                 } else if (args.size == 3) {
                     val currentRule = WordWolf.gameRules[args[1]]
 
                     if (currentRule == null) {
-                        sender.sendMessage("There is no rule like that")
+                        sender.sendMessage("해당 규칙을 찾을 수 없습니다")
                     } else {
                         WordWolf.gameRules[args[1]] = args[2]
-                        sender.sendMessage("Set rule ${args[1]} to ${WordWolf.gameRules[args[1]]}")
+                        sender.sendMessage("게임 규칙 ${args[1]}을 ${WordWolf.gameRules[args[1]]}로 변경했습니다")
                     }
                 }
             }
 
             "list" -> {
                 val nameList = WordWolf.playerList.joinToString(", ") { it.name }
-                sender.sendMessage("Current players are: $nameList")
+                sender.sendMessage("현재 참여 플레이어 목록: $nameList")
             }
 
             else -> {
-                sender.sendMessage("Incorrect usage")
+                sender.sendMessage("잘못된 사용방법입니다")
             }
         }
 
